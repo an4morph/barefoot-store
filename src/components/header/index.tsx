084852267e43068613ad6a3bfc34c3399ac2712f
an4morph <avgustin456@gmail.com>
2024-05-07 17:29:21 +0600

    add header

--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -1,11 +1,9 @@
-import { LogoOutlined } from '../logo/outlined'
 import { LogoSteps } from '../logo/steps'
 
 export default function Header(): JSX.Element {
   return (
-    <header className="h-[60px] flex-center p-3">
-      <LogoOutlined />
-      <LogoSteps />
+    <header className="h-[60px] flex-center p-2">
+      <LogoSteps className="h-full" />
     </header>
   )
 }
