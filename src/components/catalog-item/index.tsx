2590db3ab60cf74a0c223963937a66c94dc53966
an4morph <avgustin456@gmail.com>
2024-05-20 23:40:08 +0600

    change label

--- src/components/catalog-item/index.tsx
+++ src/components/catalog-item/index.tsx
@@ -21,9 +21,9 @@ export function CatalogItem({
       <div className="relative overflow-hidden rounded-xl mb-3">
         <div
           className={cn(
-            'text-sm font-medium py-0.5 px-3 absolute top-2 right-2 rounded-lg z-10',
+            'text-sm font-medium py-0.5 px-3 absolute top-2 right-2 rounded-lg z-10 text-slate-900/70',
             {
-              'bg-lime-400': status === CatalogItemStatus.stock,
+              'bg-lime-500 text-white': status === CatalogItemStatus.stock,
               'bg-slate-300': status === CatalogItemStatus.no,
               'bg-yellow-300': status === CatalogItemStatus.order,
             }
@@ -45,7 +45,7 @@ export function CatalogItem({
           })}
         />
       </div>
-      <div className="flex space-between gap-4">
+      <div className="flex space-between sm:gap-4 flex-col sm:flex-row">
         <div className="grow">
           <h2>{brand}</h2>
           <data
@@ -55,7 +55,7 @@ export function CatalogItem({
             {price} сом
           </data>
         </div>
-        <div className="text-right">
+        <div className="sm:text-right">
           {anotherColors.length ? (
             <div className="text-slate-400 text-sm">
               {anotherColors.length} цветов
